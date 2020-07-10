c5e3cac8474eb3e4138ced1caa5e1e0a5dd853d1
Valen Yamamoto <valenryamamoto@gmail.com>
2020-07-10 12:28:34 -0700

    Working bash script to call python script

--- run_matrix_mult.sh
+++ run_matrix_mult.sh
@@ -5,7 +5,6 @@ echo "Output file: $OUTPUT_FILE"
 touch $OUTPUT_FILE
 touch_pid=$!
 wait $touch_pid
-ls ~/matrix-multiply-optimization/
 for x in $@; do
-	python3 get_matrix_stats.py $x  >> "~/matrix-multiply-optimization/$OUTPUT_FLIE"
+	python3 get_matrix_stats.py $x  >> "$OUTPUT_FILE"
 done
